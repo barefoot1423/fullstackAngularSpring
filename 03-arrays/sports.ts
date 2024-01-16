667262272228dbdd5c7e5930a44a5597f7ffc4ed
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-15 21:44:14 -0500

    for loops

---
+++ 03-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let i = 0; i < sportsOne.length; i++) {
+    console.log(sportsOne[i]);
+}
+
+for (let tempSport of sportsOne) {
+    console.log(tempSport);
+}
