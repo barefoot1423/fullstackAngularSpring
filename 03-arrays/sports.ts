3724a45fa0a663d6230088bd9e8f497b830d2112
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-15 21:47:28 -0500

    add items into the array

--- 03-arrays/sports.ts
+++ 03-arrays/sports.ts
@@ -4,6 +4,10 @@ for (let i = 0; i < sportsOne.length; i++) {
     console.log(sportsOne[i]);
 }
 
+
+sportsOne.push("Baseball");
+sportsOne.push("Bike");
+
 for (let tempSport of sportsOne) {
     console.log(tempSport);
 }
