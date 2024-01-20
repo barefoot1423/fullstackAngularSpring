d4d20549bf4b0d9d6608499811c5424e8b797337
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-19 22:50:18 -0500

    test

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SalesPersonListComponent {
 
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@test.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@test.com", 40000),
+    new SalesPerson("Cal", "Son", "cla.son@test.com", 90000),
+    new SalesPerson("Joseph", "Cho", "joseph.cho@test.com", 60000),
+  ]
 }
