2a12a172d81f2218f64f867cfa60c6e8c24c0dd6
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-21 22:06:19 -0500

    ready to run the backend

--- backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.ecommerce.springbootecommerce.dao;
 import com.ecommerce.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCatetory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
